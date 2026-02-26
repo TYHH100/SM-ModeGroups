9c1447e83fb3f705a205b1d98c503b729014ea63
TYHH100 <71576227+TYHH100@users.noreply.github.com>
2026-02-26 23:49:02 +0800

    lnit

---
+++ extension/extension.h
@@ -0,0 +1,67 @@
+#ifndef _MODEGROUP_EXTENSION_H_
+#define _MODEGROUP_EXTENSION_H_
+
+#include "smsdk_config.h"
+#include <IShareSys.h>
+#include <IPluginSys.h>
+#include <IForwardSys.h>
+#include <IHandleSys.h>
+#include <ICvar.h>
+#include <IKeyValues.h>
+#include <vector>
+#include <string>
+
+using namespace SourceMod;
+
+/**
+ * @brief Mode information structure
+ */
+struct ModeInfo
+{
+	std::string name;
+	std::vector<std::string> pluginFiles;      // Relative paths from plugins folder
+	std::map<std::string, std::string> cvars;
+	std::vector<std::string> commands;
+};
+
+/**
+ * @brief ModeGroup extension class
+ */
+class ModeGroupExtension : public SDKExtension, public IPluginsListener
+{
+public:
+	ModeGroupExtension();
+	virtual ~ModeGroupExtension();
+
+	// SDKExtension overrides
+	virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	virtual void SDK_OnAllLoaded();
+	virtual void SDK_OnUnload();
+	virtual void SDK_OnPauseChange(bool paused);
+
+	// IPluginsListener overrides
+	virtual void OnPluginLoaded(IPlugin *plugin);
+	virtual void OnPluginUnloaded(IPlugin *plugin);
+
+	// Public methods
+	bool LoadConfig();
+	bool SwitchToMode(const char *modeName);
+	const char *GetCurrentMode() const { return m_CurrentMode.c_str(); }
+	const std::vector<ModeInfo> &GetModes() const { return m_Modes; }
+
+private:
+	void CollectPluginsFromDir(const char *relativeDir, std::vector<std::string> &outFiles, bool recursive);
+	void UnloadCurrentModePlugins();
+	void LoadModePlugins(const std::vector<std::string> &files);
+	void ApplyModeSettings(const ModeInfo &mode);
+	void ClearPluginList();
+
+	std::vector<ModeInfo> m_Modes;
+	std::string m_CurrentMode;
+	std::vector<IPlugin *> m_LoadedPlugins;   // Plugins loaded by current mode
+	bool m_IsSwitching;                         // Flag to distinguish our own loads
+};
+
+extern ModeGroupExtension g_ModeGroup;
+
+#endif // _MODEGROUP_EXTENSION_H_
---
+++ extension/smsdk_config.h
@@ -0,0 +1,23 @@
+#ifndef _SM_SDK_CONFIG_H_
+#define _SM_SDK_CONFIG_H_
+
+#define SMEXT_CONF_NAME			"ModeGroup"
+#define SMEXT_CONF_DESCRIPTION	"Plugin group switcher based on config files"
+#define SMEXT_CONF_VERSION		"1.0.0.0"
+#define SMEXT_CONF_AUTHOR		"TYHH100"
+#define SMEXT_CONF_URL			"https://github.com/TYHH100"
+#define SMEXT_CONF_LOGTAG		"MODEGROUP"
+#define SMEXT_CONF_LICENSE		""
+#define SMEXT_CONF_DATESTRING	__DATE__
+
+#define SMEXT_LINK(name) \
+	extern "C" { \
+		SourceMod::IExtension *GetSMExtAPI() { return (SourceMod::IExtension *)&name; } \
+	}
+
+#define SMEXT_ENABLE_FORWARDSYS
+#define SMEXT_ENABLE_HANDLESYS
+#define SMEXT_ENABLE_PLUGINSYS
+#define SMEXT_ENABLE_CORESYS
+
+#endif // _SM_SDK_CONFIG_H_
